2c0910e7ce5699105b402aacb025c4abaede30fd
katene30 <katenejack@gmail.com>
2023-05-16 17:31:33 +1200

    added father, mother, siblings

--- familytree/admin.py
+++ familytree/admin.py
@@ -14,7 +14,7 @@ class PersonForm(forms.ModelForm):
 
     class Meta:
         model = Person
-        fields = ['first_name', 'last_name', 'birth_date', 'death_date', 'is_me']
+        fields = ['first_name', 'last_name', 'birth_date', 'death_date', 'is_me', 'mother', 'father', 'siblings']
 
 class PersonAdmin(admin.ModelAdmin):
     form = PersonForm
