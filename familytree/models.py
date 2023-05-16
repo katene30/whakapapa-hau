03376d80e0e1a10ce267ce99e567bb68b799dbd5
katene30 <katenejack@gmail.com>
2023-05-16 15:10:02 +1200

    inital commit with basic models

---
+++ familytree/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    death_date = models.DateField(null=True, blank=True)
+
+class Relationship(models.Model):
+    from_person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='from_person')
+    to_person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='to_person')
+    relationship_type = models.CharField(max_length=100)
