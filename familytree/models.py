6aacde8bb682bb4db81ae762db2f6de65288d2d6
katene30 <katenejack@gmail.com>
2023-05-16 16:16:39 +1200

    first name last name and relationship choices

--- familytree/models.py
+++ familytree/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 
 class Person(models.Model):
-    name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     birth_date = models.DateField()
     death_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.first_name} {self.last_name}"
 
     class Meta:
         verbose_name_plural = "people"
 
 class Relationship(models.Model):
+    RELATIONSHIP_CHOICES = [
+        ('parent', 'Parent'),
+        ('child', 'Child'),
+        ('sibling', 'Sibling'),
+        ('spouse', 'Spouse')
+    ]
+
     from_person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='from_person')
     to_person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='to_person')
-    relationship_type = models.CharField(max_length=100)
+    relationship_type = models.CharField(max_length=20, choices=RELATIONSHIP_CHOICES)
