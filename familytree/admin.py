b9176fb81397c7862e0321397573884dfb5b436b
katene30 <katenejack@gmail.com>
2023-05-16 16:40:04 +1200

    date input for birth_date

--- familytree/admin.py
+++ familytree/admin.py
@@ -1,6 +1,17 @@
+from django import forms
 from django.contrib import admin
 
 from .models import Person, Relationship
 
-admin.site.register(Person)
+class PersonForm(forms.ModelForm):
+    birth_date = forms.DateField(input_formats=["%d/%m/%Y"])
+
+    class Meta:
+        model = Person
+        fields = ['first_name', 'last_name', 'birth_date', 'death_date']
+
+class PersonAdmin(admin.ModelAdmin):
+    form = PersonForm
+
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Relationship)
