87d913a45cce8077d4f39091543813f5e0ef399b
katene30 <katenejack@gmail.com>
2023-05-16 21:48:03 +1200

    added children and models.SETNULL

--- familytree/admin.py
+++ familytree/admin.py
@@ -14,7 +14,7 @@ class PersonForm(forms.ModelForm):
 
     class Meta:
         model = Person
-        fields = ['first_name', 'last_name', 'birth_date', 'death_date', 'is_me', 'mother', 'father', 'siblings']
+        fields = ['first_name', 'last_name', 'birth_date', 'death_date', 'is_me', 'mother', 'father', 'siblings', 'children']
 
 class PersonAdmin(admin.ModelAdmin):
     form = PersonForm
--- familytree/models.py
+++ familytree/models.py
@@ -7,12 +7,15 @@ class Person(models.Model):
     death_date = models.DateField(null=True, blank=True)
     is_me = models.BooleanField(default=False)
 
-    father = models.ForeignKey('self', on_delete=models.CASCADE, related_name='related_father', null=True, blank=True)
-    mother = models.ForeignKey('self', on_delete=models.CASCADE, related_name='related_mother', null=True, blank=True)
-    
+    # Don't delete cascaded
+    father = models.ForeignKey('self', on_delete=models.SET_NULL, related_name='related_father', null=True, blank=True)
+    mother = models.ForeignKey('self', on_delete=models.SET_NULL, related_name='related_mother', null=True, blank=True)
+
     # Ask Haydn,
     # How to choose multiple models of self
-    siblings = models.ManyToManyField('self', related_name='related_siblings', blank=True, symmetrical=False)
+    siblings = models.ManyToManyField('self', related_name='related_siblings', blank=True, null=True, symmetrical=False)
+
+    children = models.ManyToManyField('self', related_name='related_children', blank=True, null=True, symmetrical=False)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
