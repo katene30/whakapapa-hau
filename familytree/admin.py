56094838b270a66dc23801c97f62e1597ae6d6db
katene30 <katenejack@gmail.com>
2023-05-18 21:09:47 +1200

    added half siblings and conditions

--- familytree/admin.py
+++ familytree/admin.py
@@ -24,7 +24,7 @@ class PersonForm(forms.ModelForm):
 class PersonAdmin(admin.ModelAdmin):
     form = PersonForm
 
-    readonly_fields = ["children_list", "siblings_list"]
+    readonly_fields = ["children_list", "siblings_list", "half_siblings_list"]
 
     # description functions like a model field's verbose_name
     @admin.display(description="Children")
@@ -36,15 +36,25 @@ class PersonAdmin(admin.ModelAdmin):
             ((person.first_name, person.last_name) for person in Person.objects.filter(Q(mother = instance) | Q(father = instance))),
         )
     
-    # description functions like a model field's verbose_name
     @admin.display(description="Siblings")
     def siblings_list(self, instance):
         # TODO: Extract filter
-        return format_html_join(
-            mark_safe("<br>"),
-            "{} {}",
-            ((person.first_name, person.last_name) for person in Person.objects.filter(Q(mother = instance.mother) | Q(father = instance.father)).exclude(Q(id=instance.id) | Q(is_me=True) | Q(mother__isnull=True) | Q(father__isnull=True))),
-        )
+        if(instance.mother is not None and instance.father is not None):
+            return format_html_join(
+                mark_safe("<br>"),
+                "{} {}",
+                ((person.first_name, person.last_name) for person in Person.objects.filter(Q(mother = instance.mother) & Q(father = instance.father)).exclude(Q(id=instance.id) | Q(mother__isnull=True) | Q(father__isnull=True))),
+            )
+    
+    @admin.display(description="Half-Siblings")
+    def half_siblings_list(self, instance):
+        # TODO: Extract filter
+        if(instance.mother is not None and instance.father is not None):
+            return format_html_join(
+                mark_safe("<br>"),
+                "{} {}",
+                ((person.first_name, person.last_name) for person in Person.objects.filter(Q(mother = instance.mother) ^ Q(father = instance.father)).exclude(Q(id=instance.id) | Q(mother=instance) | Q(father=instance))),
+            )
 
 
 
