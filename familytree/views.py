73f0a73dc33ccd47c7be480465af1ec74ca96c94
katene30 <katenejack@gmail.com>
2023-05-18 23:34:16 +1200

    added siblings to template

--- familytree/views.py
+++ familytree/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Person
+
 
 def home(request):
     person = Person.objects.get(is_me=True)
 
+    # TODO: Needs refactoring
+    siblings = Person.objects.filter(Q(mother = person.mother) & Q(father = person.father)).exclude(Q(id=person.id) | Q(mother__isnull=True) | Q(father__isnull=True))
+
 
     context = {
         'person': person,
+        'siblings': siblings
     }
     return render(request, 'home.html', context)
